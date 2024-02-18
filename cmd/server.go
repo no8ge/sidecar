e8ffe7921363efbeaee45a2f7adfd1e501dbbfa6
lunzhou <lunzhou@yunify.com>
2024-02-18 17:58:07 +0800

    feat: server cmd

--- cmd/server.go
+++ cmd/server.go
@@ -1,8 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
@@ -10,11 +9,20 @@ func init() {
 	rootCmd.AddCommand(serverCmd)
 }
 
+func server() {
+
+	r := gin.Default()
+	r.GET("/healthz", func(c *gin.Context) {
+		c.String(200, "Hello, Geektutu")
+	})
+	r.Run(":8080")
+}
+
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Print the version number of Hugo",
 	Long:  `All software has versions. This is Hugo's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+		server()
 	},
 }
